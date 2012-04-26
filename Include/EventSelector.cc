1797a0c907e78c454b3a825aaeaca86017b7e5f1
andriusj <>
2012-04-26 13:30:51 +0000

    Dielectron selection wrapper class

---
+++ Include/EventSelector.cc
@@ -0,0 +1,129 @@
+#include "EventSelector.hh"
+#include <TLorentzVector.h>
+
+// ---------------------------------------------------------------
+
+DielectronSelector_t::DielectronSelector_t(TSelectionType_t set_selection, ElectronEnergyScale *escale) :
+  fSelection(set_selection),
+  fEScale(escale),
+  fTotalCandidates(0), fCandidatesGoodEta(0), fCandidatesGoodEt(0),
+  fCandidatesHLTMatched(0), fCandidatesIDPassed(0), fCandidatesMassAbove10(0)
+{
+}
+
+// ---------------------------------------------------------------
+
+bool DielectronSelector_t::testDielectron_default(mithep::TDielectron *dielectron, TEScaleCorrection_t applyEScale, ULong_t leadingTriggerObjectBit, ULong_t trailingTriggerObjectBit) {
+
+  fTotalCandidates++;
+
+  bool ok=kFALSE;
+  do {
+    // Exclude ECAL gap region and cut out of acceptance electrons
+    if((fabs(dielectron->scEta_1)>kECAL_GAP_LOW) && (fabs(dielectron->scEta_1)<kECAL_GAP_HIGH)) break;
+    if((fabs(dielectron->scEta_2)>kECAL_GAP_LOW) && (fabs(dielectron->scEta_2)<kECAL_GAP_HIGH)) break;
+    if((fabs(dielectron->scEta_1) > 2.5)       || (fabs(dielectron->scEta_2) > 2.5))       break;  // outside eta range? Skip to next event...
+  
+    fCandidatesGoodEta++;
+
+    //
+    // Energy scale corrections for data
+    // NOTE: the electrons and dielectron 4-vectors are updated, the supercluster quantities are not
+    //
+    Double_t scEt1 = dielectron->scEt_1;
+    Double_t scEt2 = dielectron->scEt_2;
+    // Electron energy scale correction
+    if(applyEScale==_escaleData) {
+      if (!fEScale) {
+	std::cout << "Error in testDielectron_default: escaleData is requested, but the pointer is null\n";
+	throw 2;
+      }
+
+      double corr1 = fEScale->getEnergyScaleCorrection(dielectron->scEta_1);
+      double corr2 = fEScale->getEnergyScaleCorrection(dielectron->scEta_2);
+      scEt1 = dielectron->scEt_1 * corr1;
+      scEt2 = dielectron->scEt_2 * corr2;
+
+      TLorentzVector ele1; 
+      ele1.SetPtEtaPhiM(dielectron->pt_1,dielectron->eta_1,dielectron->phi_1,0.000511);
+      ele1 *= corr1;
+      dielectron->pt_1  = ele1.Pt();
+      dielectron->eta_1 = ele1.Eta();
+      dielectron->phi_1 = ele1.Phi();
+      
+      TLorentzVector ele2; 
+      ele2.SetPtEtaPhiM(dielectron->pt_2,dielectron->eta_2,dielectron->phi_2,0.000511);
+      ele2 *= corr2;
+      dielectron->pt_2  = ele2.Pt();
+      dielectron->eta_2 = ele2.Eta();
+      dielectron->phi_2 = ele2.Phi();
+      
+      TLorentzVector vDiEle = ele1+ele2;            
+      dielectron->mass = vDiEle.M();
+      dielectron->pt   = vDiEle.Pt();
+      dielectron->y    = vDiEle.Rapidity();
+      dielectron->phi  = vDiEle.Phi(); 
+    }
+       	  
+    // requirements on BOTH electrons
+    // For DY ET cuts are asymmetric:
+    if( ! ( (scEt1>20 && scEt2>10) || (scEt1>10 && scEt2>20) ) ) break;
+    
+    fCandidatesGoodEt++;
+
+    // Both electrons must match trigger objects. At least one ordering
+    // must match
+    if( ! ( 
+	   (dielectron->hltMatchBits_1 & leadingTriggerObjectBit && 
+	    dielectron->hltMatchBits_2 & trailingTriggerObjectBit )
+	   ||
+	   (dielectron->hltMatchBits_1 & trailingTriggerObjectBit && 
+	    dielectron->hltMatchBits_2 & leadingTriggerObjectBit ) ) ) break;
+    
+    fCandidatesHLTMatched++;
+
+    // Other cuts to both electrons
+    
+    // The Smurf electron ID package is the same as used in HWW analysis
+    // and contains cuts like VBTF WP80 for pt>20, VBTF WP70 for pt<10
+    // with some customization, plus impact parameter cuts dz and dxy
+    if(!passSmurf(dielectron)) break;
+
+    fCandidatesIDPassed++;
+
+    // loose mass window 
+    if( dielectron->mass < 10 ) break;
+
+    fCandidatesMassAbove10++;
+
+    ok=kTRUE; // selection PASSED
+
+  } while(0); // fictitious loop
+  return ok;
+}
+
+
+// ---------------------------------------------------------------
+
+std::ostream& DielectronSelector_t::printInfo(std::ostream &out) {
+  char buf[20];
+  out << "DielectronSelector (selection=" << DielectronSelector_t::selectionName(fSelection) << ")\n";
+  const char *format="%10u";
+  sprintf(buf,format,fTotalCandidates);
+  out << "Total number of dielectron candidates " << buf << "\n";
+  sprintf(buf,format,fCandidatesGoodEta);
+  out << "Total candidates with good eta        " << buf << "\n";
+  sprintf(buf,format,fCandidatesGoodEt);
+  out << "Total candidates with good Et         " << buf << "\n";
+  sprintf(buf,format,fCandidatesHLTMatched);
+  out << "Total candidates HLT matched          " << buf << "\n";
+  sprintf(buf,format,fCandidatesIDPassed);
+  out << "Total candidates ID passed            " << buf << "\n";
+  sprintf(buf,format,fCandidatesMassAbove10);
+  out << "Total candidates with mass > 10       " << buf << "\n";
+  return out;
+}
+
+// ---------------------------------------------------------------
+
+// ---------------------------------------------------------------
