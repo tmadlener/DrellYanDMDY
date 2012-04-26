41b13415518ab8a178c19145973b2525041ce17c
andriusj <>
2012-04-26 13:15:38 +0000

    reorganization of variables

--- Include/DYTools.hh
+++ Include/DYTools.hh
@@ -12,13 +12,32 @@
 
 namespace DYTools {
 
+  // ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+  // Global variable: analysis is either 1D or 2D
+  // 1D analysis collapses the rapidity binning
+  // The default choice of binnings for 1D and 2D
+  //   assign correct values to 
+  //             nMassBins, massBinLimits, nYBins
+  // ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+
+  const int study2D=1;
+
+  // ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+  // ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+
+
+  // Global parameters
+  const Double_t kECAL_GAP_LOW  = 1.4442;
+  const Double_t kECAL_GAP_HIGH = 1.566;
+  const double etMinLead  = 20;
+  const double etMinTrail = 10;
+
+
+
   // Constants that define binning in mass and rapidity
   // Note: bin zero is underflow, overflow is neglected
-  const Double_t kGAP_LOW  = 1.4442;
-  const Double_t kGAP_HIGH = 1.566;
-
-  const int nMassBins2D = 7;
-  const double massBinLimits2D[nMassBins2D+1] = 
+  const int _nMassBins2D = 7;
+  const double _massBinLimits2D[_nMassBins2D+1] = 
     {0, // first bin is underflow
      20, 30, 45, 60, 120, 200, 1500
     }; // overflow is very unlikely, do not account for it
@@ -27,114 +46,11 @@ namespace DYTools {
   // in rapidity.
   const double yRangeMin =  0.0;
   const double yRangeMax =  2.5;
-  const int nYBins[nMassBins2D] = 
+  const int _nYBins2D[_nMassBins2D] = 
     { 25,// underflow, binned like first mass bin 
       25, 25, 25, 25, 25, 10,
     }; // overflow is neglected
-  // 
-  // 
-  // Unfolding matrix binning
-  int getNumberOf2DBins(){
-    int result = 0;
-    for(int i=0; i<nMassBins2D; i++)
-      result += nYBins[i];
-    return result;
-  }
 
-  // Largest number of Ybins
-  int getYBinsMax(){
-    int nYBMax=nYBins[0];
-    for (int i=1; i<nMassBins2D; i++)
-      if (nYBins[i]>nYBMax) nYBMax=nYBins[i];
-    return nYBMax;
-  }
-  // Bin limits in rapidity for particular mass slice
-  double *getYBinLimits2D(int massBin){
-    double *result = 0;
-    if( massBin < 0 || massBin >= nMassBins2D ) {
-      return result;
-    }
-    int nYBinsThisSlice = nYBins[massBin];
-    result = new double[nYBinsThisSlice+1];
-    double delta = (yRangeMax - yRangeMin)/double(nYBinsThisSlice);
-    for(int i=0; i<nYBinsThisSlice; i++){
-      result[i] = yRangeMin + i * delta;
-    }
-    result[nYBinsThisSlice] = yRangeMax;
-    return result;
-  }
-//   // Note: this HAS TO BE the total number of all 2D bins, that is
-//   // the sum of the contents of the nYBins array above
-//   const int nUnfoldingBins = 160;
-  
-  // Functions that return mass and rapidity bin index
-  int findMassBin2D(double m){
-    
-    int result =-1;
-    for(int ibin=0; ibin < nMassBins2D; ibin++){
-      if( m >= massBinLimits2D[ibin] && m < massBinLimits2D[ibin+1]) {
-	result = ibin;
-	break;
-      }
-    }
-    
-    return result;
-    
-  }
-  
-  int findAbsYBin2D(int massBin, double y){
-    
-    int result = -1;
-    if(massBin < 0 || massBin > nMassBins2D) return result;
-
-    double absY = fabs(y);
-    
-    int nYBinsThisMassRange = nYBins[massBin];
-    // Make the array large enough to accommodate any Y binning
-    double yBinLimits[5000];
-    double width = (yRangeMax - yRangeMin)/double(nYBinsThisMassRange);
-    for(int i=0; i<nYBinsThisMassRange; i++){
-      yBinLimits[i] = yRangeMin + i * width;
-    }
-    // The line below could have been done in the loop above,
-    // but it is here done separately to avoid rounding uncertainty.
-    yBinLimits[nYBinsThisMassRange] = yRangeMax;
-    
-    for(int i=0; i < nYBinsThisMassRange; i++){
-      if( absY >= yBinLimits[i] && absY < yBinLimits[i+1]) {
-	result = i;
-	break;
-      }
-    }
-    
-    return result;
-  }
-  
-  // This function finds a unique 1D index for (index_m, index_y) pair
-  int findIndexFlat(int massBin, int yBin){
-    
-    int result = -1;
-    if( massBin < 0 || massBin > nMassBins2D || yBin < 0 || yBin > nYBins[massBin] )
-      return result;
-    
-    result = 0;
-    for(int i=0; i< massBin; i++)
-      result += nYBins[i];
-    
-    result += yBin;
-    
-    return result;
-  }
-
-  // This function finds what is the maximum number of Y bins
-  // among the all mass slices.
-  int findMaxYBins(){
-    int result = 0;
-    for(int i=0; i<nMassBins2D; i++)
-      if( result < nYBins[i])
-	result = nYBins[i];
-    return result;
-  }
   
   // ------------- 2011 and 2010 content is below ---------------
 
@@ -150,26 +66,33 @@ namespace DYTools {
   // Define mass binning
   //
   // 2010 mass binning
-  const int nMassBins13 = 13;
-  const double massBinLimits13[nMassBins13+1] = 
+  const int _nMassBins13 = 13;
+  const double _massBinLimits13[_nMassBins13+1] = 
     {15,20,30,40,50,60,76,86,96,106,120,150,200,600}; // 13 bins
+  const int _nYBins13[_nMassBins13] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
   // 2011 mass binning
-  const int nMassBins2011 = 40;
-  const double massBinLimits2011[nMassBins2011+1] = 
+  const int _nMassBins2011 = 40;
+  const double _massBinLimits2011[_nMassBins2011+1] = 
     {15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 64, 68, 72, 76, 
      81, 86, 91, 96, 101, 106, 110, 115, 120, 126, 133, 141, 
      150, 160, 171, 185, 200, 220, 243, 273, 320, 380, 440, 
      510, 600, 1000, 1500}; // 40 bins
+  const int _nYBins2011[_nMassBins2011] = { 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+  };
 
-  const int nMassBinsLumi = 9;
-  const double massBinLimitsLumi[nMassBinsLumi+1] = 
+  const int _nMassBinsLumi = 9;
+  const double _massBinLimitsLumi[_nMassBinsLumi+1] = 
     {15,20,30,40,50,60,120,150,200,600}; // 9 bins with Z-peak region singled-out
+  const int _nYBinsLumi[_nMassBinsLumi] = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
-  const double etMinLead  = 20;
-  const double etMinTrail = 10;
 
-  int findMassBin(double mass, int nMassBinsLoc, const double *massBinLimitsLoc){  
+  // generic bin idx finder
+  int _findMassBin(double mass, int nMassBinsLoc, const double *massBinLimitsLoc){  
 
     int result =-1;
     for(int ibin=0; ibin < nMassBinsLoc; ibin++){
@@ -182,23 +105,133 @@ namespace DYTools {
 
   };
 
-  int findMassBin2011(double mass) { return findMassBin(mass,nMassBins2011,massBinLimits2011); }
-  int findMassBin13(double mass) { return findMassBin(mass,nMassBins13,massBinLimits13); }
-  int findMassBinLumi(double mass) { return findMassBin(mass,nMassBinsLumi,massBinLimitsLumi); }
+  // some derived bin idx finders -- for debug
+  int _findMassBin2011(double mass) { return _findMassBin(mass,_nMassBins2011,_massBinLimits2011); }
+  int _findMassBin13(double mass) { return _findMassBin(mass,_nMassBins13,_massBinLimits13); }
+  int _findMassBinLumi(double mass) { return _findMassBin(mass,_nMassBinsLumi,_massBinLimitsLumi); }
 
 // Declare mass binnings
   typedef enum { _MassBins_Undefined, _MassBins_2010, _MassBins_2011, _MassBins_2011_Lumi, _MassBins_2011_2D } TMassBinning_t;
 
 
-  // Declare mass binning
-  const int study2D=1;
+  // ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+  // Declare mass binning for 1D or 2D case
+  // ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
   //DYTools::TMassBinning_t massBinningSet=(study2D) ? _MassBins_2011_2D : _MassBins_2011;
-  const int nMassBins=(study2D) ? nMassBins2D : nMassBins2011;
-  const double *massBinLimits=(study2D) ? massBinLimits2D : massBinLimits2011;
+  const int nMassBins=(study2D) ? _nMassBins2D : _nMassBins2011;
+  const double *massBinLimits=(study2D) ? _massBinLimits2D : _massBinLimits2011;
+  const int *nYBins=(study2D) ? _nYBins2D : _nYBins2011;
 
+  // some analyses use 1D always
+  //const int nMassBins1D=nMassBins2011;
+  //const double *massBinLimits1D=massBinLimits2011;
+
+  // 
+  // Define functions which should be used in the code
+  //
 
   // find mass bin idx
-  int findMassBin(double mass) { return findMassBin(mass,nMassBins,massBinLimits); }
+  int findMassBin(double mass) { return _findMassBin(mass,nMassBins,massBinLimits); }
+
+
+  int findYBin(int massBin, double y){
+  
+    int result = -1;
+    if( massBin < 0 || massBin > nMassBins) return result;
+    if ( y < yRangeMin  ||  y > yRangeMax ) return result;
+
+    int nYBinsThisMassRange = nYBins[massBin];
+
+    int newCode=1;
+    if (newCode) {
+      result = int( ( y - yRangeMin )* 1.0001*nYBinsThisMassRange/( yRangeMax - yRangeMin ) );
+      double dy=((yRangeMax-yRangeMin)/double(nYBinsThisMassRange));
+      std::cout << "y=" << y << ", dy=" << dy  << ", binIdx=" << result << ", yRange=" << (result*dy) << ".." << (result*dy+dy) << "\n";
+    }
+    else {
+      // Make the array large enough to accommodate any Y binning
+      double yBinLimits[5000];
+      double width = (yRangeMax - yRangeMin)/double(nYBinsThisMassRange);
+      for(int i=0; i<nYBinsThisMassRange; i++){
+	yBinLimits[i] = yRangeMin + i * width;
+      }
+      // The line below could have been done in the loop above,
+      // but it is here done separately to avoid rounding uncertainty.
+      yBinLimits[nYBinsThisMassRange] = yRangeMax;
+      
+      for(int i=0; i < nYBinsThisMassRange; i++){
+	if( y >= yBinLimits[i] && y < yBinLimits[i+1]) {
+	  result = i;
+	  break;
+	}
+      }
+    }
+    
+    return result;
+  }
+  
+  int findAbsYBin(int massBin, double y){ return findYBin(massBin,fabs(y)); }
+  int findYBin(double mass, double y) { return findYBin(findMassBin(mass),y); }
+  int findAbsYBin(double mass, double y) { return findYBin(findMassBin(mass),fabs(y)); }
+  
+
+  // This function finds a unique 1D index for (index_m, index_y) pair
+  int findIndexFlat(int massBin, int yBin){
+    
+    int result = -1;
+    if( massBin < 0 || massBin > nMassBins || yBin < 0 || yBin > nYBins[massBin] )
+      return result;
+    
+    result = 0;
+    for(int i=0; i< massBin; i++)
+      result += nYBins[i];
+    
+    result += yBin;
+    
+    return result;
+  }
+
+
+  // 
+  // 
+  // Unfolding matrix binning
+  // info: was getNumberOf2DBins
+  int getTotalNumberOfBins(){
+    int result = 0;
+    for(int i=0; i<nMassBins; i++)
+      result += nYBins[i];
+    return result;
+  }
+
+  // Largest number of Ybins
+  int getYBinsMax(){
+    int nYBMax=nYBins[0];
+    for (int i=1; i<nMassBins; i++)
+      if (nYBins[i]>nYBMax) nYBMax=nYBins[i];
+    return nYBMax;
+  }
+
+  // Bin limits in rapidity for a particular mass slice
+  double *getYBinLimits(int massBin){
+    double *result = 0;
+    if( massBin < 0 || massBin >= nMassBins ) {
+      return result;
+    }
+    int nYBinsThisSlice = nYBins[massBin];
+    result = new double[nYBinsThisSlice+1];
+    double delta = (yRangeMax - yRangeMin)/double(nYBinsThisSlice);
+    for(int i=0; i<nYBinsThisSlice; i++){
+      result[i] = yRangeMin + i * delta;
+    }
+    result[nYBinsThisSlice] = yRangeMax;
+    return result;
+  }
+
+  // NEEDS clean up
+//   // Note: this HAS TO BE the total number of all 2D bins, that is
+//   // the sum of the contents of the nYBins array above
+//   const int nUnfoldingBins = 160;
+  
 
 
   //
@@ -339,7 +372,7 @@ namespace DYTools {
   const double nPVLimits[nPVBinCount+1] = { 0.5, 2.5, 4.5, 6.5, 8.5, 10.5, 12.5, 14.5, 16.5, 20.5, 24.5, 40.5 };
   //  1., 3., 5., 7., 9., 11., 13., 15., 17., 21., 25., 40. };
 
-  int findPUBin(int nPV) { return findMassBin(double(nPV),nPVBinCount,nPVLimits); }
+  int findPUBin(int nPV) { return _findMassBin(double(nPV),nPVBinCount,nPVLimits); }
 
   // 
   // Triggers vs run numbers
@@ -469,19 +502,17 @@ namespace DYTools {
   //
   // Barrel or endcap
   //
-  const Double_t ECAL_GAP_LOW  = 1.4442;
-  const Double_t ECAL_GAP_HIGH = 1.566;
 
   bool isBarrel(double eta){
     double result = false;
-    if(fabs(eta) <= ECAL_GAP_LOW) 
+    if(fabs(eta) <= kECAL_GAP_LOW) 
       result = true;
     return result;
   }
 
   bool isEndcap(double eta){
     double result = false;
-    if(fabs(eta) >= ECAL_GAP_HIGH && fabs(eta)<2.5 ) 
+    if(fabs(eta) >= kECAL_GAP_HIGH && fabs(eta)<2.5 ) 
       result = true;
     return result;
   }
