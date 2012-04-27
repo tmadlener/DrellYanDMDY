8f4c2cde67e131fea268bac70be6759af1f54e02
andriusj <>
2012-04-27 15:07:22 +0000

    update to selectEvents.C v.1.9

--- Selection/auxScript.C
+++ Selection/auxScript.C
@@ -1,5 +1,5 @@
 {
   gROOT->ProcessLine(".L selectEvents.C+");
-  selectEvents("../config_files/dataT3.conf"); 
+  selectEvents("../config_files/dataT3.conf","Full2011DatasetTriggers",DYTools::NORMAL); 
   //selectEvents("../config_files/dataReduced.conf"); 
 }
