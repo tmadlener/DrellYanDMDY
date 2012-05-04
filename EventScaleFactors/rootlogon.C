7b87a73e774ebf9467a50a5f5a5d685d27f83dc2
andriusj <>
2012-05-04 16:37:44 +0000

    ROOT starter script

---
+++ EventScaleFactors/rootlogon.C
@@ -0,0 +1,10 @@
+{  
+  
+  gROOT->ProcessLine(".x ../Include/rootlogon.C");
+
+  gROOT->ProcessLine(".L cutFunctions.cc+");
+  gROOT->ProcessLine(".L fitFunctionsCore.cc+");
+  gROOT->ProcessLine(".L fitFunctions.cc+");
+  gROOT->ProcessLine(".L tnpSelectEvents.hh+");
+}
+
