7bdbbea19408efb1cc25f87b59e7629781d992ce
eavdeeva <>
2012-06-13 14:17:58 +0000

    to remove .so, .d and ~ files from the package

---
+++ remove_dot-so_dot-d_tilda_files.sh
@@ -0,0 +1,35 @@
+cd Acceptance
+rm *.so *.d *~
+
+cd ../CrossSection
+rm *.so *.d *~
+
+cd ../Efficiency
+rm *.so *.d *~
+
+cd ../EventScaleFactors
+rm *.so *.d *~
+
+cd ../Fsr
+rm *.so *.d *~
+
+cd ../Include
+rm *.so *.d *~
+
+cd ../Selection
+rm *.so *.d *~
+
+cd ../Skimming
+rm *.so *.d *~
+
+cd ../Theory
+rm *.so *.d *~
+
+cd ../Unfolding
+rm *.so *.d *~
+
+cd ../YieldsAndBackgrounds
+rm *.so *.d *~
+
+cd ..
+rm *.so *.d *~
