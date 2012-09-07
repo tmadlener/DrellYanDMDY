9885b48baf64889a3c131fc92af8c9c257aaa164
andriusj <>
2012-09-07 16:01:15 +0000

    added function AdjustFor2DplotWithHeight(TCanvas)

--- Include/MitStyleRemix.hh
+++ Include/MitStyleRemix.hh
@@ -8,3 +8,13 @@ void     InitSubPad   (TPad* pad, int i);
 void     InitHist     (TH1 *hist, const char *xtit, const char *ytit  = "Number of Entries",
 		       EColor color = kBlack);
 void     SetStyle     ();
+
+
+
+inline
+void AdjustFor2DplotWithHeight(TCanvas *c) {
+  for (int i=1; i<50; i++) {
+    TPad *pad=(TPad*)c->GetPad(i);
+    if (pad) pad->SetRightMargin(1.5);
+  }
+}
