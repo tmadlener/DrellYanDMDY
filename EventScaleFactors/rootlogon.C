b36fc1d57b600e752d5d6c3a17ff682eeea0cd55
ikrav <>
2013-07-26 12:00:57 +0000

    Load new file that defines code for RooCMSShape

--- EventScaleFactors/rootlogon.C
+++ EventScaleFactors/rootlogon.C
@@ -2,6 +2,7 @@
   
   gROOT->ProcessLine(".x ../Include/rootlogon.C");
 
+  gROOT->ProcessLine(".L RooCMSShape.cc+");
   gROOT->ProcessLine(".L cutFunctions.cc+");
   gROOT->ProcessLine(".L fitFunctionsCore.cc+");
   gROOT->ProcessLine(".L fitFunctions.cc+");
