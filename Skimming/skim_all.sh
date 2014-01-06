d9c7654b4ec9473f7e2b00eb05fe1fe7482ba23f
Ilya Kravchenko <ikrav@unl.edu>
2014-01-06 17:31:09 -0600

    A variety of configs and the main shell script used in the most recent skimming round

--- Skimming/skim_all.sh
+++ Skimming/skim_all.sh
@@ -1,24 +1,51 @@
+#!/bin/bash
 # This file is an example of skimming and trimming.
 # Before doing it, one has to carefully check all the input configs.
 # The files below ARE NOT a full set
 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data1\"\) >& log-skim-data1.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data2\"\) >& log-skim-data2.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data3\"\) >& log-skim-data3.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data4\"\) >& log-skim-data4.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data5\"\) >& log-skim-data5.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data1\"\) >& log-skim-data1_wReg.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data2\"\) >& log-skim-data2_wReg.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data3\"\) >& log-skim-data3_wReg_corr.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data4\"\) >& log-skim-data4_wReg.txt 
 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zttm20\"\) >& log-skim-zttm20.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zeem20\"\) >& log-skim-zeem20.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zeem1020\"\) >& log-skim-zeem1020.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zz\"\) >& log-skim-zz.txt 
+# root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.data5\"\) >& log-skim-data5.txt 
 
-root -l -b -q TrimNtuples.C+\(\"../config_files/skim_configs/trim.input.zeem20\"\) >& log-trim-zeem20.txt 
-root -l -b -q TrimNtuples.C+\(\"../config_files/skim_configs/trim.input.zeem500\"\) >& log-trim-zeem500.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zttm1020\"\) >& log-skim-zttm1020.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zttm20\"\) >& log-skim-zttm20.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zeem20\"\) >& log-skim-zeem20_wReg.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zeem1020\"\) >& log-skim-zeem1020_wReg.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zz\"\) >& log-skim-zz.txt 
 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zeem20to500\"\) >& log-skim-zeem20to500.txt 
+#root -l -b -q TrimNtuples.C+\(\"../config_files/skim_configs/trim.input.zeem20\"\) >& log-trim-zeem20_wReg.txt 
+#root -l -b -q TrimNtuples.C+\(\"../config_files/skim_configs/trim.input.zeem500\"\) >& log-trim-zeem500.txt 
 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.ttj\"\) >& log-skim-ttj.txt 
-root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.wjets\"\) >& log-skim-wjets.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zeem20to500\"\) >& log-skim-zeem20to500_wReg.txt 
+
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.ttj\"\) >& log-skim-ttj.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.wjets\"\) >& log-skim-wjets_wReg.txt 
+
+root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.zz4l\"\) >& log-skim-zz4l.txt 
+
+#root -l -b -q TrimNtuples.C+\(\"../config_files/skim_configs/trim.input.tt2l\"\) >& log-trim-tt2l-mg.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.tt2l-mg-trimmed\"\) >& log-skim-tt2l-mg-trimmed.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.ttj700to1000\"\) >& log-skim-ttj700to1000.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.ttj1000\"\) >& log-skim-ttj1000.txt 
+
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.wz\"\) >& log-skim-wz2q2l.txt 
+
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdem2030\"\) >& log-skim-qcdem2030.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdem3080\"\) >& log-skim-qcdem3080.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdem80170\"\) >& log-skim-qcdem80170.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdem170250\"\) >& log-skim-qcdem170250.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdem250350\"\) >& log-skim-qcdem250350.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdem350\"\) >& log-skim-qcdem350.txt 
+
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdbc2030\"\) >& log-skim-qcdbc2030.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdbc3080\"\) >& log-skim-qcdbc3080.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdbc80170\"\) >& log-skim-qcdbc80170.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdbc170250\"\) >& log-skim-qcdbc170250.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdbc250350\"\) >& log-skim-qcdbc250350.txt 
+#root -l -b -q SkimNtuples.C+\(\"../config_files/skim_configs/skim.input.qcdbc350\"\) >& log-skim-qcdbc350.txt 
+
 
 
